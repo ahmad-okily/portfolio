8f5f235cbaeb238d6b40cd9991d5f8ffff97e42f
ahmad <ahmadokili061@gmail.com>
2024-12-19 16:26:23 +0200

    my project

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !subject || !message) {
+        alert('Please fill out all fields before submitting.');
+        event.preventDefault();
+    } else {
+        alert('Your message has been sent successfully!');
+    }
+});
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
